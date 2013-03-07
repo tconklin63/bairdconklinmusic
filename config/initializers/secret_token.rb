c55daa4afa9d05cc3439e2a4b9c4479b5fdc44cb
Thomas Conklin <tconklin63@gmail.com>
2013-03-07 10:50:13 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bairdconklinmusic::Application.config.secret_token = '567d1b5084f0bf75d356421336bdbac48f59d5f3b33e5c82271a8b5460f817d82555124c9c0e3d7e10ba4eeff5b03046e451441e61dddcd0a4cc80685e0a92ae'
