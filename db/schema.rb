2d525ad824fc21449279f44e9102d561a63ed0ba
Thomas Conklin <tconklin63@gmail.com>
2013-03-10 11:43:40 -0700

    Added admin field to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130309234901) do
+ActiveRecord::Schema.define(:version => 20130310181819) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(:version => 20130309234901) do
     t.string   "password"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.boolean  "admin"
   end
 
 end
