99762076f253d0be234e138ace0ff4d7d5efb11e
Thomas Conklin <tconklin63@gmail.com>
2013-03-14 10:29:48 -0700

    Added menu item for admin users

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,8 @@ class User < ActiveRecord::Base
   validates_confirmation_of :password
   validates_format_of :email, :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i, :message => "Invalid email"  
 
-  attr_protected :id, :salt
+  attr_accessible :name, :email
+  attr_protected :id, :salt, :admin
 
   attr_accessor :password, :password_confirmation
 
