30ebd2115a2c3423d881e886135e55631fabd63c
Thomas Conklin <thomasconklin@Thomass-MacBook-Pro.local>
2014-03-08 14:07:53 -0800

    Added Category model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131203042326) do
+ActiveRecord::Schema.define(:version => 20140308220557) do
+
+  create_table "categories", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "compositions", :force => true do |t|
     t.string   "name"
