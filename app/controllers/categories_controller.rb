5273192c8791dc350a97021788df38c6931864b6
Thomas Conklin <thomasconklin@Thomass-MacBook-Pro.local>
2014-03-29 20:10:54 -0700

    Added edit category page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -31,6 +31,19 @@ class CategoriesController < ApplicationController
       redirect_to "/"
     end
   end
+  
+  def edit
+    @category = Category.find(params[:id])
+  end
+  
+  def update
+    @category = Category.find(params[:id])
+    if @category.update_attributes(params[:category])
+      redirect_to :categories
+    else
+      render 'edit'
+    end
+  end
 
   
 end
